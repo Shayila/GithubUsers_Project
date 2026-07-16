13c8fb240260b03307a5bae0922730e476049b67
Shayila <zaksha9195@gmail.com>
2026-07-16 08:01:17 +0530

    Initial Commit

---
+++ GithubUsers_Project/GithubUsers_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubUsers_ProjectApp.swift
+//  GithubUsers_Project
+//
+//  Created by Shayila Kamaal on 16/07/26.
+//
+
+import SwiftUI
+
+@main
+struct GithubUsers_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GithubUsers_ProjectUITests/GithubUsers_ProjectUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  GithubUsers_ProjectUITestsLaunchTests.swift
+//  GithubUsers_ProjectUITests
+//
+//  Created by Shayila Kamaal on 16/07/26.
+//
+
+import XCTest
+
+final class GithubUsers_ProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
